1a45d956cca3f224f60e08bb25314842fe480706
Matheus <dev.paolini.matheus@gmail.com>
2022-11-17 15:02:16 -0300

    feat: Added useInViewport hook.

---
+++ src/hooks/useInViewport/index.ts
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { isIntersetionObserver } from '../../utils/isIntersetionObserver';
+
+export type Observe<T> = (element?: T | null) => void;
+
+export const intersectionObserverAPIError =
+  'The browser doesn\'t support Intersection Observer, please install a polyfill https://www.npmjs.com/package/intersection-observer.';
+
+export function useInViewport<T extends HTMLElement | null>() {
+  const [inViewport, setInViewport] = useState(false);
+
+  const innerRef = useRef<T>();
+  const observerRef = useRef<IntersectionObserver>();
+
+  const unobserve = useCallback(() => {
+    if (observerRef.current) {
+      observerRef.current.disconnect();
+    }
+  }, []);
+
+  const observe = useCallback<Observe<T>>((element) => {
+    if (element && element !== innerRef.current) {
+      unobserve();
+
+      innerRef.current = element;
+    }
+
+    if (observerRef.current && innerRef.current) {
+      observerRef.current.observe(innerRef.current as HTMLElement);
+    }
+  }, []);
+
+  const initObserver = useCallback(() => {
+    if (!isIntersetionObserver)
+      return console.error(intersectionObserverAPIError);
+
+    if (!innerRef.current) return;
+
+    observerRef.current = new IntersectionObserver((entries) => entries.forEach((item) => {
+      const isInsideViewport = item.isIntersecting;
+      setInViewport(isInsideViewport);
+    }));
+
+    observerRef.current.observe(innerRef.current);
+
+    observe();
+  }, [observe]);
+
+  useEffect(() => {
+    initObserver();
+
+    return () => {
+      observerRef.current?.disconnect()
+    }
+  }, [initObserver]);
+
+  return { observe, unobserve, inViewport };
+}
