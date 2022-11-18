9636a179d4393cedb1aab0e407d702ed8c159378
Matheus <dev.paolini.matheus@gmail.com>
2022-11-17 22:48:52 -0300

    fix: Fixed features export.

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,37 @@
 // Components
 import { For } from './components/For';
 import { Portal } from './components/Portal';
+import { Stack } from './components/Stack';
 import { Show } from './components/Visibility';
 import { Hide } from './components/Visibility';
 
 export {
   For,
+  Portal,
+  Stack,
   Show,
   Hide,
-  Portal,
 }
 
 // Hooks
+import { useBoolean } from './hooks/useBoolean';
+import { useCounter } from './hooks/useCounter';
+import { useCreateRange } from './hooks/useCreateRange';
+import { useDebounce } from './hooks/useDebounce';
+import { useInViewport } from './hooks/useInViewport';
+import { useList } from './hooks/useList';
 import { useMount } from './hooks/useMount';
 import { useUnmount } from './hooks/useUnmount';
-import { useBoolean } from './hooks/useBoolean';
-import { useDebounce } from './hooks/useDebounce';
 import { useWindowSize } from './hooks/useWindowSize';
-import { useCreateRange } from './hooks/useCreateRange';
 
 export {
   useBoolean,
-  useWindowSize,
+  useCounter,
+  useCreateRange,
+  useDebounce,
+  useInViewport,
+  useList,
   useMount,
   useUnmount,
-  useDebounce,
-  useCreateRange
+  useWindowSize,
 }
