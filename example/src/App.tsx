79a8f4292d5cf1bb9329dcca7ea4299703c841c2
Matheus <dev.paolini.matheus@gmail.com>
2022-11-17 22:44:28 -0300

    feat: Version 0.3.2

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,7 @@
 import { useInViewport } from '../../src/hooks/useInViewport';
 import { useWindowSize } from '../../src/hooks/useWindowSize';
 import { useCreateRange } from '../../src/hooks/useCreateRange';
+import { useCounter } from '../../src/hooks/useCounter';
 import { Stack } from '../../src/components/Stack';
 import { useEffect, useState } from 'react';
 
@@ -10,16 +11,20 @@ export default function App() {
 
   const { width } = useWindowSize();
 
-  console.log(width);
-
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useCounter();
 
   return (
     <Stack flexDirection="column" padding={16} isMaxHeight>
+      <div>Counter: {counter}</div>
       <div>Title 1</div>
       <div>Title 2</div>
       <div>Title 3</div>
       <div>Title 4</div>
+
+      <button onClick={setCounter.increment}>increment</button>
+      <button onClick={setCounter.decrement}>decrement</button>
+
+      <button onClick={setCounter.reset}>reset</button>
     </Stack>
   );
 }
