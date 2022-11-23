7a0490a5d0240ec53b08b2f4e5616b8ae4d87d42
Matheus <dev.paolini.matheus@gmail.com>
2022-11-23 20:30:28 -0300

    feat: Changed example app.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,11 @@ import { useWindowSize } from '../../src/hooks/useWindowSize';
 import { useCreateRange } from '../../src/hooks/useCreateRange';
 import { useCounter } from '../../src/hooks/useCounter';
 import { Stack } from '../../src/components/Stack';
+import { Stacker } from '../../src/components/Stacker';
+import { Space } from '../../src/components/Space';
 import { useEffect, useState } from 'react';
+
+const Button = () => <button style={{ width: '100%' }}>Title 1</button>
 
 export default function App() {
   const { observe, unobserve, inViewport } = useInViewport();
@@ -15,16 +19,21 @@ export default function App() {
 
   return (
     <Stack flexDirection="column" padding={16} isMaxHeight>
-      <div>Counter: {counter}</div>
-      <div>Title 1</div>
-      <div>Title 2</div>
-      <div>Title 3</div>
-      <div>Title 4</div>
+      <Stacker changeTo="column" onWidthUnder={600}>
+        <Button />
+        <Button />
+        <Space size={200} />
+        <Button />
+        <Button />
+      </Stacker>
 
-      <button onClick={setCounter.increment}>increment</button>
+      <Space axis="y" size={200} />
+      <Button />
+
+      {/* <button onClick={setCounter.increment}>increment</button>
       <button onClick={setCounter.decrement}>decrement</button>
 
-      <button onClick={setCounter.reset}>reset</button>
+      <button onClick={setCounter.reset}>reset</button> */}
     </Stack>
   );
 }
